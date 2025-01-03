11eb17b9bef350f72012ee329d2c0e8adb2d91fe
Tran Tuyen <61371510+tuyen1204@users.noreply.github.com>
2025-01-03 23:14:03 +0700

    chore: delete folder

--- Remix/my-remix-app/app/routes/contacts.$contactId.tsx
+++ Remix/my-remix-app/app/routes/contacts.$contactId.tsx
@@ -1,8 +1,9 @@
-import { json, type LoaderFunctionArgs } from "@remix-run/node";
-// existing imports
-import { getContact } from "../data";
+import { Form, json, useLoaderData } from "@remix-run/react";
+import type { FunctionComponent } from "react";
+
+import { getContact, type ContactRecord } from "../data";
 import invariant from "tiny-invariant";
-import { useLoaderData } from "@remix-run/react";
+import { LoaderFunctionArgs } from "@remix-run/node";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   invariant(params.contactId, "Missing contactId param");
@@ -12,23 +13,80 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   }
   return json({ contact });
 };
-
 export default function ContactComponent() {
   const { contact } = useLoaderData<typeof loader>();
-  if (!contact) {
-    return <p>Loading...</p>;
-  }
+
   return (
     <div id="contact">
       <div>
-        {contact.id}
         <img
           alt={`${contact.first} ${contact.last} avatar`}
           key={contact.avatar}
           src={contact.avatar}
         />
       </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter ? (
+          <p>
+            <a href={`https://twitter.com/${contact.twitter}`}>
+              {contact.twitter}
+            </a>
+          </p>
+        ) : null}
+
+        {contact.notes ? <p>{contact.notes}</p> : null}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+
+          <Form
+            action="destroy"
+            method="post"
+            onSubmit={(event) => {
+              const response = confirm(
+                "Please confirm you want to delete this record."
+              );
+              if (!response) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
-// existing code
+
+const Favorite: FunctionComponent<{
+  contact: Pick<ContactRecord, "favorite">;
+}> = ({ contact }) => {
+  const favorite = contact.favorite;
+
+  return (
+    <Form method="post">
+      <button
+        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+        name="favorite"
+        value={favorite ? "false" : "true"}
+      >
+        {favorite ? "★" : "☆"}
+      </button>
+    </Form>
+  );
+};
